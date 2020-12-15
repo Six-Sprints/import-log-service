fbf8059bcc18c0c8b32187fbc930bc21d12b90af
Karan Gujral <karan.gujral@jptokyo.co.jp>
2020-12-15 11:52:59 +0530

    Changed slug format

--- src/main/java/com/sixsprints/importlog/service/impl/ImportLogDetailsServiceImpl.java
+++ src/main/java/com/sixsprints/importlog/service/impl/ImportLogDetailsServiceImpl.java
@@ -1,11 +1,13 @@
 package com.sixsprints.importlog.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sixsprints.core.domain.ImportLogDetails;
 import com.sixsprints.core.dto.ImportLogDetailsDto;
 import com.sixsprints.core.dto.MetaData;
+import com.sixsprints.core.dto.SlugFormatter;
 import com.sixsprints.core.service.AbstractCrudService;
 import com.sixsprints.core.service.ImportLogDetailsService;
 import com.sixsprints.importlog.data.ImportLogDetailsFieldData;
@@ -35,6 +37,20 @@ public class ImportLogDetailsServiceImpl extends AbstractCrudService<ImportLogDe
   @Override
   protected ImportLogDetails findDuplicate(ImportLogDetails introScreen) {
     return ImportLogDetailsRepository.findBySlug(introScreen.getSlug());
+  }
+
+  @Override
+  protected void generateSlugIfRequired(ImportLogDetails entity) {
+    if (StringUtils.isEmpty(entity.getId())) {
+      SlugFormatter slugFromatter = slugFromatter(entity);
+      if (slugFromatter != null && slugFromatter.getCollection() != null) {
+        Long nextSequence = getNextSequence(slugFromatter.getCollection());
+        String slug = "0000000" + nextSequence.toString();
+        slug = slugFromatter.getPrefix() + slug.substring(slug.length() - 7);
+        entity.setSlug(slug);
+        entity.setSequence(nextSequence);
+      }
+    }
   }
 
 }
